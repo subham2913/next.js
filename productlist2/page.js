9a183a0d0a0814cdb43d7be74e83d6ee4511909c
Subham Tiwari <118207456+subham2913@users.noreply.github.com>
2024-09-15 12:03:27 +0530

    Add files via upload
    in this we import client component in server component

--- productlist2/page.js
+++ productlist2/page.js
@@ -1,12 +1,18 @@
+import Product from "./product";
+
 export default async function Page() {
-    let data = await fetch('https://dummyjson.com/products')
-    let {products} = await data.json()
-    console.log(products)
+    let data = await fetch('https://dummyjson.com/products');
+    let { products } = await data.json();
+    console.log(products);
+
     return (
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.title}</li>
-        ))}
-      </ul>
-    )
-  }
+        <ul>
+            {products.map((product) => (
+                <div className="flex space-x-10" key={product.id}> {/* Wrapped in a parent element */}
+                    <li>{product.title}</li>
+                    <Product price={product.price} />
+                </div>
+            ))}
+        </ul>
+    );
+}
